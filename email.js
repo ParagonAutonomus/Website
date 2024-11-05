6b5390b6436f2c9ff2bf5d3b5947485fa7644d51
samuelkimasu <skim622@asu.edu>
2024-11-05 14:24:40 -0700

    New and improved!
    Pages that needs more improvement:
    Meet the team
    Stakeholders
    Impact
    Home page

--- email.js
+++ email.js
@@ -1,19 +1,21 @@
 function sendEmail() {
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "sender@email_address.com",
-        Password: "Enter your password",
-        To: 'receiver@email_address.com',
-        From: "sender@email_address.com",
-        Subject: "Sending Email using javascript",
-        Body: "Well that was easy!!",
-        Attachments: [
-            {
-                name: "File_Name_with_Extension",
-                path: "Full Path of the file"
-            }]
-    })
-        .then(function (message) {
-            alert("Mail has been sent successfully")
-        });
+    event.preventDefault();  // Prevents default form submission
+
+    var params = {
+        name: document.getElementById("fname").value,
+        email: document.getElementById("mail").value,
+        message: document.getElementById("subject").value
+    };
+    const serviceID = "service_sa926ag";
+    const templateID = "template_q9p6lna";
+
+    emailjs.send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("fname").value = "";
+            document.getElementById("mail").value = "";
+            document.getElementById("subject").value = "";
+            console.log(res);
+            alert("Your message was sent successfully!");
+        })
+        .catch((err) => console.log(err));
 }
